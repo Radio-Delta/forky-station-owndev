04f3cbdcb1c8101df6692bf3c503c3ad11f1927c
Radio-Delta <breaktablemajor@proton.me>
2026-08-16 21:25:40 +0100

    Changed Access Breaker and EMAG to be 6 charges

--- Content.Shared/Charges/Components/LimitedChargesComponent.cs
+++ Content.Shared/Charges/Components/LimitedChargesComponent.cs
@@ -18,7 +18,7 @@ public sealed partial class LimitedChargesComponent : Component
     ///     The max charges this action has.
     /// </summary>
     [DataField, AutoNetworkedField]
-    public int MaxCharges = 6;
+    public int MaxCharges = 6; // funky changed charges 3 -> 6
 
     /// <summary>
     /// Last time charges was changed. Used to derive current charges.
